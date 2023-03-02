6420f799981759c218808f8ffa0f8fb61a19ab24
Genesis Melina Correa Martinez <genemeli21@outlook.com>
2023-03-01 20:04:50 -0500

    imcorporar numero 1

---
+++ numero1.py
@@ -0,0 +1,16 @@
+num1 = 5
+num = 2
+
+def suma(numero1, numero2):
+    total = numero1 + numero2
+    return total
+ 
+
+def resta(numero1, numero2):
+    toal = numero1 - numero2
+    return total 
+    
+
+resultado= suma(num1, num)
+resultado = resta(num1, num)
+print ('el resultado es :', resultado)
